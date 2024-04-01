a600eefaa7ec52549586cf2a3c6d000472a2cb51
iamkyle <kyleaglsabat@gmail.com>
2024-04-01 18:01:59 +0800

    Responsive homepage

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
     this.style.height = this.scrollHeight + "px";
   });
 });
+
+// TOGGLE MENU
+const burgerIcon = document.getElementById("burger-icon");
+const navLinks = document.getElementById("nav-links");
+
+function toggleNavLinks() {
+  if (window.innerWidth <= 834) {
+    if (navLinks.style.display !== "block") {
+      navLinks.style.display = "block";
+    } else {
+      navLinks.style.display = "none";
+    }
+  } else {
+    navLinks.style.display = "block";
+  }
+}
+
+burgerIcon.addEventListener("click", function () {
+  toggleNavLinks();
+});
+
+window.addEventListener("resize", function () {
+  toggleNavLinks();
+});
